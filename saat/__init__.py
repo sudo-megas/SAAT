e3ac955c450057f1983ec821327dceddae81ddab
sudo-megas <gonzalorodriguez761@gmail.com>
2026-07-28 07:52:07 +0300

    Milestone 21d: layout pass for longer strings

--- saat/__init__.py
+++ saat/__init__.py
@@ -14,4 +14,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "1.7.6"
+__version__ = "1.7.8"
